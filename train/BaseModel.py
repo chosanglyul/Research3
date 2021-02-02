6e8dce10a6c063ddac15c9108171077ceccfba2d
chosanglyul <chosanglyul@gmail.com>
2021-02-02 22:03:11 +0900

    Delete sincos

--- train/BaseModel.py
+++ train/BaseModel.py
@@ -1,25 +1,36 @@
 import torch
+from datetime import datetime, timedelta
 from pytorch_lightning import LightningModule
 
 class BaseModel(LightningModule):
-    def __init__(self, loss_fn, metrics_fn=[], optim=torch.optim.Adam, lr=1e-3, device=None):
+    def __init__(self, loss_fn, metrics_fn=[], optim=torch.optim.Adam, lr=1e-3):
         super().__init__()
-        if device is None: self.dev = ('cuda' if torch.cuda.is_available() else 'cpu')
-        else: self.dev = device
+        self.time = {}
         self.metrics_fn = metrics_fn
         self.loss_fn = loss_fn
         self.optim = optim
         self.lr = lr
     
-    def forward(self, X):
-        return X
-        
     def configure_optimizers(self):
         return self.optim(self.parameters(), lr=self.lr)
     
+    def forward(self, X):
+        return X
+    
     def forward_batch(self, batch):
         X, y = batch
-        return self.forward(X), y
+        sttime = datetime.now()
+        t = self.forward(X)
+        self.time['time(ms)/forward'] = (datetime.now()-sttime) / timedelta(milliseconds=1)
+        return t, y
+    
+    def backward(self, loss, optimizer, optimizer_idx, *args, **kwargs):
+        sttime = datetime.now()
+        loss.backward(*args, **kwargs)
+        self.time['time(ms)/backward'] = (datetime.now()-sttime) / timedelta(milliseconds=1)
+        
+    def on_after_backward(self):
+        self.log_dict(self.time, on_step=False, on_epoch=True)
     
     def log_metrics(self, h, y, step_name):
         metrics = {'{}/{}'.format(name, step_name) : fn(h, y) for fn, name in self.metrics_fn}
